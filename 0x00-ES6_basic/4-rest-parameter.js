be77e21ae9bc2a1c3540a9eac7db2b59777699b0
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-01-23 22:47:40 +0300

    Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...theArgs) {
-  let count = 0;
-  for (let i = 0; i < theArgs.length; i + 1) {
-    count += 1;
-  }
-  return count;
+  return theArgs.length;
 }
