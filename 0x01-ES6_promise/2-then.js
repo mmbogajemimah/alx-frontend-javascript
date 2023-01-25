64bdfcfb6b6267919fecbc85839edab86728cf93
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-01-25 14:34:46 +0300

    Append three handlers to the function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,11 @@
 /*  In this example, the handleResponseFromAPI function takes a
 single argument promise, which is a promise object. */
 export default function handleResponseFromAPI(promise) {
-  const response = { status: 200, body: 'success' };
   promise
     /* The first handler is attached using the .then() method.
     This handler will be called when the promise is resolved,
     and it returns an object with the attributes status and body. */
-    .then(() => (response))
+    .then(() => ({ status: 200, body: 'success' }))
     /* The second handler is attached using the .catch() method.
     This handler will be called when the promise is rejected,
     and it returns an empty error object. */
@@ -14,5 +13,5 @@ export default function handleResponseFromAPI(promise) {
     /* The last handler is attached using the .finally() method.
     This handler will be called after the promise is settled
     (resolved or rejected) and it logs "Got a response from the API" to the console.  */
-    .finally(() => { console.log('Got a response from the API'); });
+    //.finally(() => { console.log('Got a response from the API'); });
 }
