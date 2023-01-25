643a95c2a4c92c8ee32a256189953888697e5e46
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-01-25 16:51:12 +0300

    resolve all promises and log body firstName lastName to the console.

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+// The Promise.all() method is used to wait for all promises to resolve and
+// it takes an array of promises as an argument.
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+  /* The .then() method is used to attach a callback function that will be called
+  with an array of the resolved values of the input promises. The callback function
+  uses destructuring assignment to extract the body, firstName and lastName properties
+  of the objects and logs their values to the console. */
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    })
+    .catch(() => { console.log('Signup system offline'); });
+}
