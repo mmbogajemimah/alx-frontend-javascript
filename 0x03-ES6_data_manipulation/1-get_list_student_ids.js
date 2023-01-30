9cc857c16b18239f492c2ee7d3eab8a3afbba54e
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-01-31 00:16:57 +0300

    Mapping Objects

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,5 @@ export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  const studentId = students.map((element) => `${element.id}`);
-  const numberId = studentId.map((elem) => Number(elem));
-  return numberId;
+  return students.map((element) => element.id);
 }
