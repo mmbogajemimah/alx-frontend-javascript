948ef415ff87c502189b7febe6a14b385385a0a2
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-01-31 09:46:52 +0300

    a function getStudentsByLocation that returns an array of objects who are located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
-  const sameCity = students.filter(student => student.location === city);
+  const sameCity = students.filter((student) => student.location === city);
   return sameCity;
 }
