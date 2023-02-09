c99caa498fe1730974ae015976b37940c66b4aba
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-02-09 16:41:37 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,4 +1,17 @@
+// CREATING A FUNCTION
 function printTeacher(firstName, lastName) {
     return "".concat(firstName[0], ". ").concat(lastName);
 }
-console.log(printTeacher("John", "Doe"));
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,30 @@ interface printTeacherFunction {
 }
 //console.log(printTeacher("John", "Doe"));
 
+// WRITING A CLASS
+interface StudentConstructor {
+  new(firstName: string, lastName: string): Student;
+}
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName
+  }
+}
+
